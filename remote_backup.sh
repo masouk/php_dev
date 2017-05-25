b15099cbb709e53efdcc682ec1776bb3b31bb925
masouk <anoint216371@hotmail.com>
2017-05-25 11:52:23 +0800

    linux shell
    backup db command

---
+++ remote_backup.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+#備份 sun_db
+DBName=sun_new_55
+BackupPath=/home/sun_db/
+DBbinPath=/usr/local/bin/mysqldump
+
+if ${DBbinPath} --opt --extended-insert=false -h 127.0.0.1 --port 3306 -uxxxxx -xxxxxxxxx ${DBName} > ${BackupPath}${DBName}"/"${DBName}`date "+%Y-%m-%d"`".sql" ;
+then
+	find ${BackupPath}${DBName}"/" -mtime +3 -exec rm {} \;
+else
+	exit
+fi
+
